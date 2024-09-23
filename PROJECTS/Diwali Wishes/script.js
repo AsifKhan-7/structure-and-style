3caf0edaa5d164380b19c6c1619177cf7f5479ad
Asif Khan <beingasifjamshed777@gmail.com>
2024-09-23 00:14:54 -0700

    Diwali Wishesh Project Complete

---
+++ PROJECTS/Diwali Wishes/script.js
@@ -0,0 +1,7 @@
+const receiverName = document.querySelector("#receiver");
+
+const senderName = document.querySelector("#sender");
+
+receiverName.innerText = new URLSearchParams(location.search).get("receiver");
+
+senderName.innerText = new URLSearchParams(location.search).get("sender") || "Asif Khan";
