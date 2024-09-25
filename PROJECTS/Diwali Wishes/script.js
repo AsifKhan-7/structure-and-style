0fa8858149a312fb420a623b77d42fdee1e327c8
Asif Khan <beingasifjamshed777@gmail.com>
2024-09-25 11:51:25 -0700

    Projects Folder Untracked

--- PROJECTS/Diwali Wishes/script.js
+++
@@ -1,7 +0,0 @@
-const receiverName = document.querySelector("#receiver");
-
-const senderName = document.querySelector("#sender");
-
-receiverName.innerText = new URLSearchParams(location.search).get("receiver");
-
-senderName.innerText = new URLSearchParams(location.search).get("sender") || "Asif Khan";
